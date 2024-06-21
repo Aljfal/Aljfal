c655ad15c9d6d2c71dccca7b4deb94654d39ae5f
Aljfal <161582094+Aljfal@users.noreply.github.com>
2024-06-21 16:51:20 +0300

    Add files via upload

--- Pages-Sayfalar/index.js
+++ Pages-Sayfalar/index.js
@@ -1,101 +1,22 @@
-import ResturantAbout from "@/Components/Resturants/About";
-import ResturantBestDishes from "@/Components/Resturants/Bestdishes";
-import Comments from "@/Components/Resturants/Comments";
-import ResturantFooter from "@/Components/Resturants/Footer";
-import ResturantGallery from "@/Components/Resturants/Gallery";
-import ResturantMenu from "@/Components/Resturants/Menu";
-import ResturantNav from "@/Components/Resturants/Nav";
-import ResturantPricingAndReservations from "@/Components/Resturants/PricingAndReservation";
-import ResturantTeam from "@/Components/Resturants/Team";
+import MainAdminNav from "@/Components/Admin/MainAdmin/MainAdminNav";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
-import { useSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-export default function Resturant() {
+export default function Admin() {
   const { data: session, status } = useSession();
-  const [Gallarys, setGallarys] = useState([]);
-  const [User, setUser] = useState();
-  const [Resturant, setResturant] = useState("");
+  const [OwnerRequests, setOwnerRequests] = useState([]);
+  const [changed, setChanged] = useState(true);
 
   const router = useRouter();
-  const id = router.query;
-  const getDataResturant = async () => {
-    try {
-      const response = await axios.get(`/api/Resturants/${id.id}`, {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.SECRET_TOKEN}`,
-        },
-      });
 
-      if (response.status >= 200 && response.status < 300) {
-        setResturant(response.data.data);
-      }
-    } catch (error) {
-      console.error("Error fetching Restaurants:", error);
-    }
-  };
-  const getdata = async () => {
-    try {
-      // Make a GET request to the API
-      const response = await axios.get(`/api/Gallarys/${id.id}`, {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.SECRET_TOKEN}`,
-        },
-      });
-
-      // Update the state with the fetched data
-      if (response.status >= 200 && response.status < 300) {
-        setGallarys(response.data.data);
-      }
-    } catch (error) {
-      console.error("Error fetching Gallarys:", error);
-      // Handle error (e.g., show an error message)
-    }
-  };
-
-  // Fetch data when the component mounts
-  useEffect(() => {
-    getdata();
-    getDataResturant();
-  }, []);
-
-  const [Employees, setEmployees] = useState([]);
-  const getdataemployee = async () => {
-    try {
-      // Make a GET request to the API
-      const response = await axios.get(`/api/Employee/${id.id}`, {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.SECRET_TOKEN}`,
-        },
-      });
-
-      // Update the state with the fetched data
-      if (response.status >= 200 && response.status < 300) {
-        setEmployees(response.data.data);
-      }
-    } catch (error) {
-      console.error("Error fetching Employees:", error);
-      // Handle error (e.g., show an error message)
-    }
-  };
-
-  useEffect(() => {
-    getdataemployee();
-  }, []);
-
-  const [Meals, setMeals] = useState([]);
   // Fetch data function
-  const getdatameal = async () => {
+  const getData = async () => {
     try {
-      // Make a GET request to the API
-      const response = await axios.get(`/api/Meals/${id.id}`, {
+      const response = await axios.get("/api/OwnerRequest", {
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
@@ -103,112 +24,160 @@ export default function Resturant() {
         },
       });
 
-      // Update the state with the fetched data
       if (response.status >= 200 && response.status < 300) {
-        setMeals(response.data.data);
+        setOwnerRequests(response.data.data);
       }
     } catch (error) {
-      console.error("Error fetching Meals:", error);
-      // Handle error (e.g., show an error message)
+      console.error("Error fetching OwnerRequests:", error);
     }
   };
-
-  useEffect(() => {
-    getdatameal();
-  }, []);
-
-  const getdatalog = async () => {
-    try {
-      // Make a GET request to the API
-      const response = await axios.get(`/api/Login/${session.user.id}`, {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.SECRET_TOKEN}`,
-        },
-      });
-
-      // Update the state with the fetched data
-      if (response.status >= 200 && response.status < 300) {
-        setUser(response.data.data);
-      }
-    } catch (error) {
-      console.error("Error fetching Gallarys:", error);
-      // Handle error (e.g., show an error message)
-    }
-  };
-
-  // Fetch data when the component mounts
-  useEffect(() => {
-    if (session) getdatalog();
-  }, [session]);
-  const [comments, setComments] = useState([]);
-  // Fetch data function
-  const getdataComments = async () => {
-    try {
-      // Make a GET request to the API
-      const response = await axios.get(`/api/Comments/${id.id}`, {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.SECRET_TOKEN}`,
-        },
-      });
-
-      // Update the state with the fetched data
-      if (response.status >= 200 && response.status < 300) {
-        const sortedComments = response.data.data.sort(
-          (a, b) => b.Likes - a.Likes
-        );
-        setComments(sortedComments);
-      }
-    } catch (error) {
-      console.error("Error fetching Comments:", error);
-      // Handle error (e.g., show an error message)
-    }
-  };
-  const [changed, setChanged] = useState(true);
-  function handleChanged() {
-    setChanged(!changed);
-  }
 
   useEffect(() => {
     if (changed) {
-      getdataComments();
-      getDataResturant();
-      getdatameal();
+      getData();
       setChanged(false);
     }
   }, [changed]);
+  function handleChanged() {
+    setChanged(!changed);
+  }
+  function datefunc(date) {
+    const dateObject = new Date(date);
 
+    // Arrange for Turkish format (day/month/year)
+    const formattedDate =
+      `${dateObject.getDate().toString().padStart(2, "0")}/` +
+      `${(dateObject.getMonth() + 1).toString().padStart(2, "0")}/` +
+      `${dateObject.getFullYear().toString().substring()}`;
+
+    return formattedDate;
+  }
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const delOwnerRequests = async (id) => {
+    try {
+      const response = await axios.delete(`/api/OwnerRequest/${id}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.SECRET_TOKEN}`,
+        },
+      });
+      if (response.status >= 200 && response.status < 300) {
+        setIsOpen(false);
+        handleChanged()
+      }
+    } catch (error) {
+      console.error("Error deleting OwnerRequests:", error);
+      // Handle error
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.keyCode === 27) {
+        changed();
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return (
     <>
-      <ResturantNav Logo={Resturant?.Logo} Name={Resturant?.Name} />
-      <ResturantAbout About={Resturant?.description} />
-      {
-        //        <ResturantBestDishes/>
-      }
-      <ResturantMenu Meals={Meals} user={User} changed={handleChanged} />
-      <ResturantTeam Employees={Employees} />
-      <ResturantGallery Gallarys={Gallarys} />
-      <Comments
-        id={id}
-        user={User}
-        Comments={comments}
-        changed={handleChanged}
-        Resturant={Resturant}
-      />
+      <MainAdminNav />
+      <div className="content">
+        <main>
+          <div className="header">
+            <div className="left">
+              <h1>Messages</h1>
+              <ul className="breadcrumb">
+                <li>
+                  <a href="#">Messages</a>
+                </li>
+                <li>
+                  <a href="#" className="active">
+                    List Messages
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </main>
 
-      <ResturantPricingAndReservations id={id} User={User} />
-
-      <ResturantFooter
-        About={Resturant?.description}
-        Logo={Resturant?.Logo}
-        Phone={Resturant?.PhoneNumber}
-        Mail={Resturant?.Mail}
-        Adress={Resturant?.Adress}
-        Meals={Meals}
-      />
+        <section className="wrapper">
+          <main className="row12 title">
+            <ul>
+              <li>date</li>
+              <li>Name</li>
+              <li>E-Mail</li>
+              <li>Phone</li>
+              <li>Changes</li>
+            </ul>
+          </main>
+          {OwnerRequests?.map((item) => (
+            <article className="  row12 nfl" key={item._id}>
+              <ul>
+                <li>
+                  <a>{datefunc(item.date)}</a>
+                </li>
+                <li>{item.Name}</li>
+                <li>{item.Mail}</li>
+                <li>{item.Phone}</li>
+                <li>
+                  <button
+                    onClick={() => setIsOpen(true)}
+                    className=" px-10 font-bold text-red-600"
+                  >
+                    delete
+                  </button>
+                  {isOpen && (
+                    <div
+                      className={`custom-popup-overlay ${
+                        isOpen ? "fade-in" : "fade-out"
+                      }`}
+                    >
+                      <div className=" fixed inset-0 flex items-center justify-center bg-black bg-opacity-30 text-black">
+                        <div className="custom-popup-innerC rounded-lg bg-white p-12  w-5/4 max-w-3xl ">
+                          <form>
+                            <div className="relative flex items-center justify-center h-12 w-12 mx-auto mb-6  bg-red-200 rounded-full ">
+                              <button
+                                onClick={() => setIsOpen(false)}
+                                className="custom-close-icon   "
+                              >
+                                <FontAwesomeIcon
+                                  icon={faClose}
+                                  className="iconclose"
+                                />
+                              </button>
+                            </div>
+                            <div className="text-center text-2xl font-semibold mb-6">
+                              Do you want to delete it?
+                            </div>
+                            <div className="flex justify-center transform transition-transform duration-200 ease-in-out hover:scale-105 hover:shadow-2xl">
+                              <button
+                                type="button"
+                                className="w-32 px-6 py-3 text-lg text-white bg-red-600 rounded-md shadow-lg hover:bg-red-700 transition duration-200"
+                                onClick={() => delOwnerRequests(item._id)}
+                              >
+                                Delete
+                              </button>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </li>
+              </ul>
+              <ul className="more-content">
+                <li>{item.Message}</li>
+              </ul>
+            </article>
+          ))}
+        </section>
+      </div>
     </>
   );
 }
